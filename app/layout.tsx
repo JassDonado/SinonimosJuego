99f6bb187bc945c8177469a57cfc1a9faec7bd18
JassDonado <jfdonado@unibarranquilla.edu.co>
2026-04-24 16:56:23 -0500

    titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-    title: "Sinónimos en Contexto",
+    title: "Aprendamos Sinonimos",
     description: "Aprende sinónimos jugando",
     icons: {
         icon: "/favicon.ico",
